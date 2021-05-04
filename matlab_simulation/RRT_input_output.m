eed8c8eb711dbd91f2c4d32e26303f06d04a6a75
Giulio <giulioturrisi@hotmail.it>
2021-05-04 18:30:37 +0200

    adding input-output planning (with and without incremental input)

--- matlab_simulation/RRT_input_output.m
+++ matlab_simulation/RRT_input_output.m
@@ -1,4 +1,4 @@
-classdef RRT_primitives < handle
+classdef RRT_input_output < handle
     %RRT_PRIMITIVES Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -6,14 +6,14 @@ classdef RRT_primitives < handle
         nodes
         dt
         map_limit
-        primitives
         goal
         map
+        k
      
     end
     
     methods
-        function obj = RRT_primitives(initial_state,sampling_time,limit,goal,map)
+        function obj = RRT_input_output(initial_state,sampling_time,limit,goal,map)
             %RRT_PRIMITIVES Construct an instance of this class
             %   Detailed explanation goes here
             obj.nodes = [initial_state(1) initial_state(2) initial_state(3) 0 0 0];
@@ -21,27 +21,9 @@ classdef RRT_primitives < handle
             obj.map_limit = limit;
             obj.goal = goal;
             obj.map = map;
+            obj.k = 0.3162;
             
-            %primitives v, w
-            forward = [1,0]
-            %backward = [-1*0,0]
-            turn_left = [0, -1]*0.3
-            turn_right = [0, 1]*0.3
-            arc_left_forward = [1, -1*0.3]
-            arc_right_forward = [1, 1*0.3]
-            arc_left_min_forward = [1, -0.5*0.3]
-            arc_right_min_forward = [1, 0.5*0.3]
-            arc_left_forward_min = [0.5, -0.5*0.3]
-            arc_right_forward_min = [0.5, 0.5*0.3]
-            %arc_left_backward = [-1*0, -1*0.3]
-            %arc_right_backward = [-1*0, 1*0.3]
-            %arc_left_backward_min = [-1*0, -0.5*0.3]
-            %arc_right_backward_min = [-1*0, 0.5*0.3]
-
-            obj.primitives = [forward;turn_left;turn_right;arc_left_forward;arc_right_forward;
-                          arc_left_forward_min;arc_right_forward_min;
-                          arc_left_min_forward;arc_right_min_forward]
-            
+ 
         end
         
         function add_nodes(obj,new_node)
@@ -76,7 +58,7 @@ classdef RRT_primitives < handle
                 x = node(1);
                 y = node(2);
                 theta = node(3); 
-                distance = sqrt((x-new_node(1))^2 + (y-new_node(2))^2 + 2*(theta-new_node(3))^2);
+                distance = sqrt((x-new_node(1))^2 + (y-new_node(2))^2 + 1*(theta-new_node(3))^2);
                 if(distance < best_distance)
                     best_distance = distance;
                     near_index = k;
@@ -108,33 +90,17 @@ classdef RRT_primitives < handle
             best_index = 1;
             best_distance = 10000;
             best_node = [0 0 0];
-            best_control = [];
-            v_w = obj.primitives();
-            for i = 1:length(obj.primitives)
-                    for d = 1:80
-                        if(d < 40)
-                            v = v_w(i,1)*d*0.1;
-                            w = v_w(i,2)*d*0.1;
-                        else
-                            v = v_w(i,1)*rand();
-                            w = v_w(i,2)*rand();
-                        end
-                        
-
-                        x_new = near_node(1) + v*cos(near_node(3))*obj.dt;
-                        y_new = near_node(2) + v*sin(near_node(3))*obj.dt;
-                        theta_new = near_node(3) + w*obj.dt;
-
-                        distance = sqrt((x_new-desired_node(1))^2 + (y_new-desired_node(2))^2 + 2*(theta_new-desired_node(3))^2);
-                        if(distance < best_distance & obj.check_collision([x_new y_new theta_new]))
-                        %if(distance < best_distance)    
-                            best_distance = distance;
-                            best_index = i;
-                            best_node = [x_new y_new theta_new];
-                            best_control = [v w];
-                        end
-                    end
-            end
+            best_control = [obj.k obj.k];
+            
+            b = 0.05;
+            u1 = obj.k*(desired_node(1) - near_node(1));
+            u2 = obj.k*(desired_node(2) - near_node(2));
+            x_new = near_node(1) + u1*obj.dt;
+            y_new = near_node(2) + u2*obj.dt;
+            theta_new = near_node(3) + (u2*cos(near_node(3)) - u1*sin(near_node(3)))*obj.dt/b;% + w*obj.dt;
+            
+            best_node = [x_new y_new theta_new]
+       
             
             new_node = [best_node near_index best_control];
         end
@@ -162,7 +128,7 @@ classdef RRT_primitives < handle
             y = node_to_check(2);
             theta = node_to_check(3);
             
-            radius = 0.25;
+            radius = 0.3;
             %height = 0.2;
             %width = 0.2;
             scale = 20;
@@ -171,7 +137,9 @@ classdef RRT_primitives < handle
             bottom = [int16(x*scale), int16((y - radius)*scale)];
             left = [int16((x - radius)*scale), int16(y*scale)];
             right = [int16((x + radius)*scale), int16(y*scale)];
-            if(abs(x) > 18 | abs(y) > 18)
+            if(x < 0 | y < 0)
+                good = 0;
+            elseif(abs(x) > 18 | abs(y) > 18)
                 good = 0;
             elseif(obj.map(top(1),top(2)) < 250 | obj.map(bottom(1),bottom(2)) < 250 | obj.map(left(1),left(2)) < 250 | obj.map(right(1),right(2)) < 250)
                 good = 0;
--- matlab_simulation/main.m
+++ matlab_simulation/main.m
@@ -1,26 +1,39 @@
 %x y theta x_dot y_dot theta_dot
-state_robot = [4 10 0 0 0 0]
-dt = 0.5;
+state_robot = [4 10 0 0 0 0];
+dt = 0.1;
 
 image = imread('map.pgm');
-%imshow(image)
 imageNorm = double(image)/255;
 imageOccupancy = 1 - imageNorm;
 map = occupancyMap(imageOccupancy,20);
-%show(map)
-%%N.B 0.0039 è free, sopra tutto occupato o incerto
+%N.B 0.0039 è free, sopra tutto occupato o incerto
 
-RRT = RRT_primitives(state_robot,dt,[20,20],[10,8],image)
-%main for RRT
+%LQR for input-output planning (if used)
+%A = ;
+%B = [1];
+%Q = [10];
+%R = 100;
+%[K,S,e] = lqr(A,B,Q,R);
+
+A = [1 dt; 0 1];
+B = [dt;1];
+Q = eye(2)*5;
+R = 10;
+[K,S,e] = dlqr(A,B,Q,R);
+
+%RRT choice
+%RRT = RRT_input_output(state_robot,dt,[20,20],[9,5],image);
+RRT = RRT_input_output_deltaInput(state_robot,dt,[20,20],[9,5],image);
+%RRT = RRT_primitives(state_robot,dt,[20,20],[10,4],image);
+
+%RRT loop
 for j = 1:100000
     j
-    %RRT.nodes
     desired_node = RRT.sample();
     near_index = RRT.find_nearest(desired_node);
     new_node = RRT.choose_primitives(near_index,desired_node);
     %%check collision
     good = RRT.check_collision(new_node);
-    %good = 1;
     if(good == 1)
         RRT.add_nodes(new_node);
     end
@@ -29,66 +42,98 @@ for j = 1:100000
     if(finish == 1)
         path = RRT.take_path(new_node(4));
         break;
-    end;
+    end
 end
 
 
 
-
-k1 = 15;
-k2 = 15;
-k3 = 1;
+%parameters needed for control loop
+k1 = 10;
+k2 = 10;
+k3 = 5;
 state_robot(1) = state_robot(1);% + 0.1;
 state_robot(2) = state_robot(2);% - 0.1;
 state_robot(3) = state_robot(3);% + 0.8;
 real_robot = [state_robot(1),state_robot(2),state_robot(3)];
 
+%augment path for bigger control_dt
 size_path = size(path);
-xq = 0:0.01:size_path(1);
-path_x = interp1(path(:,1),xq,'spline');
-path_y = interp1(path(:,2),xq,'spline');
-path_theta = interp1(path(:,3),xq,'spline');
-path_v = interp1(path(:,5),xq,'spline');
-path_w = interp1(path(:,6),xq,'spline');
-
-
+control_dt = 0.01;
+xq = 0:control_dt:size_path(1);
+path_x = fliplr(interp1(path(:,1),xq,'spline'));
+path_y = fliplr(interp1(path(:,2),xq,'spline'));
+path_theta = fliplr(interp1(path(:,3),xq,'spline'));
+path_v = fliplr(interp1(path(:,5),xq,'linear'));
+path_w = fliplr(interp1(path(:,6),xq,'linear'));
 path = [path_x' path_y' path_theta' path_theta' path_v' path_w'];
 size_path = size(path);
 
-dt = 0.01;
-for d = 1:size_path(1)
-    if(size_path(1) - d == 0)
+
+%grey to rgb mab
+rgbImage = cat(3, image, image, image);
+
+%main loop control
+for d = 2:size_path(1)-1
+    if(d == size_path(1))
         break;
     end
-    near_node = path(size_path(1) - d,:);
-    %state_robot(1) = state_robot(1) + near_node(5)*cos(state_robot(3))*dt;
-    %state_robot(2) = state_robot(2) + near_node(5)*sin(state_robot(3))*dt; 
-    %state_robot(3) = state_robot(3) + near_node(6)*dt;
+    near_node = path(d,:);
+
     
+    
+    %nonlinear control
     e1 = cos(state_robot(3))*(near_node(1) - state_robot(1)) + sin(state_robot(3))*(near_node(2) - state_robot(2));
     e2 = -sin(state_robot(3))*(near_node(1) - state_robot(1)) + cos(state_robot(3))*(near_node(2) - state_robot(2));
     e3 = near_node(3) - state_robot(3);
     
     u1 = -k1*e1;
-    u2 = -k2*near_node(5)*(sin(e3)/e3)*e2 - k3*e3;
+    if(e3 == 0)
+        u2 = -k2*near_node(5)*(sin(e3)/(e3 + 0.1))*e2 - k3*e3;
+    else
+        u2 = -k2*near_node(5)*(sin(e3)/(e3))*e2 - k3*e3;
+    end
     
     v = near_node(5)*cos(e3) - u1;
     w = near_node(6) - u2;
     
+
+    
+    %input-output lin
+    x_vel = (near_node(1) - path(d - 1,1))/dt;
+    y_vel = (near_node(2) - path(d - 1,2))/dt;
+    u1_io = k1*0.5*(near_node(1) - state_robot(1)) + x_vel;
+    u2_io = k1*0.5*(near_node(2) - state_robot(2)) + y_vel;
+    v = cos(state_robot(3))*u1_io + sin(state_robot(3))*u2_io;
+    w = -sin(state_robot(3))*u1_io/0.1 + cos(state_robot(3))*u2_io/0.1;
+    
+    
+    %integration
     state_robot(1) = state_robot(1) + v*cos(state_robot(3))*dt;
     state_robot(2) = state_robot(2) + v*sin(state_robot(3))*dt; 
     state_robot(3) = state_robot(3) + w*dt;
     
-    image(int16(state_robot(1)*20),int16(state_robot(2)*20)) = 0;
+    
+    %draw path and inflated robot
+    x = near_node(1);
+    y = near_node(2);
+    %x = state_robot(1);
+    %y = state_robot(2);
+    radius = 0.15;
+    
+    rgbImage = insertShape(rgbImage,'circle',[int16(near_node(2)*20) int16(near_node(1)*20) radius],'LineWidth',2, 'Color', 'blue');
+    rgbImage(int16(x*20),int16(y*20),1) = 255;
+    rgbImage(int16(x*20),int16(y*20),2) = 0;
+    rgbImage(int16(x*20),int16(y*20),3) = 0;
+    
+    
+    
     real_robot = vertcat(real_robot,[state_robot(1),state_robot(2),state_robot(3)]);
 end
 
+%plotting
 plot(path(:,1),path(:,2));
 hold on;
 plot(real_robot(:,1),real_robot(:,2))
 
-imshow(image);
-
-%xq = 0:0.01:size_path(1);
-%path_x = interp1(path(:,1),xq,'spline');
-%path_y = interp1(path(:,2),xq,'spline');
+J = imrotate(rgbImage,90);
+imshow(J);
