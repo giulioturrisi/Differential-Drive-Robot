c53f9337d279caf5bc73ed3c219a3bcf166f4915
enne-windows <giulioturrisi@hotmail.it>
2021-07-18 19:13:08 +0200

    update driver motor

--- ros2_ws/src/driver_motor/src/motor.cpp
+++ ros2_ws/src/driver_motor/src/motor.cpp
@@ -24,6 +24,7 @@
 #include "tf2/utils.h"
 
 
+//TO RUN sudo -s, then . install/setup.bash 
 
 
 using std::placeholders::_1;
@@ -35,7 +36,8 @@ class MotorController : public rclcpp::Node{
   public:
     
 
-    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr subscription_motors_commands;
+  
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_motors_commands;
 
 
     MotorController()
@@ -43,8 +45,8 @@ class MotorController : public rclcpp::Node{
     {
         //publisher_motors_commands = this->create_subscription<std_msgs::msg::Float64MultiArray>("motors", 1);
 
-        subscription_motors_commands = this->create_subscription<std_msgs::msg::Float64MultiArray>(
-      "motors", 1, std::bind(&MotorController::controller_callback, this, _1));
+        subscription_motors_commands = this->create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel", 1, std::bind(&MotorController::controller_callback, this, _1));
 
         gpioInitialise();
         //left
@@ -103,8 +105,10 @@ class MotorController : public rclcpp::Node{
         if(isLeft == 1) {
             //left
             gpioPWM(13,(int)duty_cycle_forw);
-            gpioWrite(26,1);
-            gpioWrite(20,0);
+            //gpioWrite(26,1);
+            //gpioWrite(20,0);
+            gpioWrite(26,0);
+            gpioWrite(20,1);
         }
         else{
             //right
@@ -122,8 +126,10 @@ class MotorController : public rclcpp::Node{
         if(isLeft == 1) {
             //left
             gpioPWM(13,(int)duty_cycle_back);
-            gpioWrite(26,0);
-            gpioWrite(20,1);
+            //gpioWrite(26,0);
+            //gpioWrite(20,1);
+            gpioWrite(26,1);
+            gpioWrite(20,0);
         }
         else{
             //right
@@ -135,20 +141,20 @@ class MotorController : public rclcpp::Node{
     }
 
     
-    void controller_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg) const{
+    void controller_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const{
 
-        //re_decoder right(22, 23, update_rightEncoder);
-        //re_decoder left(27, 17, update_leftEncoder);
+        re_decoder right(22, 23, update_rightEncoder);
+        re_decoder left(27, 17, update_leftEncoder);
         //re_decoder right(22, 23, update_rightEncoder);
         
-        int W = 10;
+        int W = 1;
 
         //RCLCPP_INFO(this->get_logger(), "boh '%f'", 0.0);
         
-        double duty_cycle_Left = 0;
-        double vel_d_Left = msg->data[0];
+        double duty_cycle_Left =  0;
+        double vel_d_Left = msg->linear.x*10;
 
-        //RCLCPP_INFO(this->get_logger(), "vel_d_Left '%f'", vel_d_Left);
+        //RCLCPP_INFO(this->get_logger(), "VEL DESIRED '%f'", vel_d_Left);
 
 
         //RCLCPP_INFO(this->get_logger(), "tickLeft '%i'", tickLeft);
@@ -156,36 +162,40 @@ class MotorController : public rclcpp::Node{
         
         
 
-        double speed_Left = 0;
-        double speed_last_Left = 0;
-        double speed_filtered_Left = 0;
+        float speed_Left = 0;
+        float speed_last_Left = 0;
+        float speed_filtered_Left = 0;
 
-        double error_vel_Left = 0;
-        double error_acc_Left = 0;
-        double error_vel_integral_Left = 0;
+        float error_vel_Left = 0;
+        float error_acc_Left = 0;
+        float error_vel_integral_Left = 0;
 
-        double angle_rad_Left = 0;
-        double old_angle_rad_Left = 0;
+        float angle_rad_Left = 0;
+        float old_angle_rad_Left = 0;
 
 
-        double duty_cycle_Right = 0;
-        double vel_d_Right = msg->data[1];
+        float duty_cycle_Right = 0;
+        float vel_d_Right = -msg->linear.x*10;
 
-        double speed_Right = 0;
-        double speed_last_Right = 0;
-        double speed_filtered_Right = 0;
+        float speed_Right = 0;
+        float speed_last_Right = 0;
+        float speed_filtered_Right = 0;
 
-        double error_vel_Right = 0;
-        double error_acc_Right = 0;
-        double error_vel_integral_Right = 0;
+        float error_vel_Right = 0;
+        float error_acc_Right = 0;
+        float error_vel_integral_Right = 0;
+        //RCLCPP_INFO(this->get_logger(), "error_vel_integral_Right beginning '%f'", error_vel_integral_Right);
         
 
-        double angle_rad_Right = 0;
-        double old_angle_rad_Right = 0;
+        float angle_rad_Right = 0;
+        float old_angle_rad_Right = 0;
+
+        tickRight = 0;
+        tickLeft = 0;
         
         //RCLCPP_INFO(this->get_logger(), "vel_d_Right '%f'", vel_d_Right);
 
-        for(int i = 0; i < 1; i++) {
+        for(int i = 0; i < 50; i++) {
 
             auto start = std::chrono::high_resolution_clock::now();
 
@@ -193,26 +203,30 @@ class MotorController : public rclcpp::Node{
             old_angle_rad_Left = angle_rad_Left;
             angle_rad_Left = (tickLeft*0.20)*3.14159/180;
 
-            speed_Left = (angle_rad_Left - old_angle_rad_Left)/0.001;
-            
-
+            //calculation speed
+            speed_Left = (angle_rad_Left - old_angle_rad_Left)/0.002;
             speed_filtered_Left = (speed_last_Left + speed_Left)/2;
             speed_last_Left = speed_Left;
             
+
+            //calculation errors
             error_vel_Left = vel_d_Left - speed_filtered_Left;
-            error_acc_Left = (speed_last_Left + speed_filtered_Left)/0.001;
+            error_acc_Left = (speed_last_Left + speed_filtered_Left)/0.002;
             error_vel_integral_Left += error_vel_Left;
+            
 
-            //duty_cycle_Left = W*(10*error_vel_Left + 0.1*error_vel_integral_Left - 0*error_acc_Left);
-            if(vel_d_Right > 0)
-                duty_cycle_Left = 45 +  vel_d_Right;
+            //calculation duty cycle
+            duty_cycle_Left = W*(30*error_vel_Left + 0.5*error_vel_integral_Left - 0*error_acc_Left);
+            RCLCPP_INFO(this->get_logger(), "Desired '%f'", vel_d_Left);
+            RCLCPP_INFO(this->get_logger(), "Actual '%f'", speed_filtered_Left);
+
+            //move
+            if(vel_d_Left > 0)
+                duty_cycle_Left = 5*0 +  duty_cycle_Left;
             else
-                duty_cycle_Left = -45 +  vel_d_Right;
-
+                duty_cycle_Left = -5*0 +  duty_cycle_Left;
             if(duty_cycle_Left > 255) duty_cycle_Left = 255;
             if(duty_cycle_Left < -255) duty_cycle_Left = -255;
-
-        
             if(duty_cycle_Left < 0) {
                 backward(1,-duty_cycle_Left);
             }
@@ -220,37 +234,39 @@ class MotorController : public rclcpp::Node{
                 forward(1,duty_cycle_Left);
             }
 
-            //RCLCPP_INFO(this->get_logger(), "qui 1 '%f'", 1.0);
+
 
             ////////////////////////////////////////
 
 
             //RIGHT
-
             old_angle_rad_Right = angle_rad_Right;
             angle_rad_Right = (tickRight*0.11)*3.14159/180;
 
-            speed_Right = (angle_rad_Right - old_angle_rad_Right)/0.001;
-            
-
+            //calculation speed
+            speed_Right = (angle_rad_Right - old_angle_rad_Right)/0.002;
             speed_filtered_Right = (speed_last_Right + speed_Right)/2;
             speed_last_Right = speed_Right;
             
+            //calculation errors
             error_vel_Right = vel_d_Right - speed_filtered_Right;
-            error_acc_Right = (speed_last_Right + speed_filtered_Right)/0.001;
+            error_acc_Right = (speed_last_Right + speed_filtered_Right)/0.002;
             error_vel_integral_Right += error_vel_Right;
 
-            //duty_cycle_Right = W*(100*error_vel_Right + 0.1*error_vel_integral_Right - 2*error_acc_Right);
+            //RCLCPP_INFO(this->get_logger(), "error_vel_integral_Right '%f'", 0.1*error_vel_integral_Right);
+
+            //RCLCPP_INFO(this->get_logger(), "error_vel_Right '%f'", error_vel_Right);
+
+            //calculation duty cycle
+            duty_cycle_Right = W*(30*error_vel_Right + 0.5*error_vel_integral_Right - 2*error_acc_Right*0);
+            
+            //move
             if(vel_d_Right > 0)
-                duty_cycle_Right = 45 + vel_d_Right;
+                duty_cycle_Right = 5*0 + duty_cycle_Right;
             else
-                duty_cycle_Right = -45 + vel_d_Right;
-
-
+                duty_cycle_Right = -5*0 + duty_cycle_Right;
             if(duty_cycle_Right > 255) duty_cycle_Right = 255;
             if(duty_cycle_Right < -255) duty_cycle_Right = -255;
-
-        
             if(duty_cycle_Right < 0) {
                 backward(0,-duty_cycle_Right);
             }
@@ -258,31 +274,38 @@ class MotorController : public rclcpp::Node{
                 forward(0,duty_cycle_Right);
             }
 
-            //RCLCPP_INFO(this->get_logger(), "qui 2 '%f'", 2.0);
-
-            double Ts_fake = 0;
-            /*while (true){
+            
+            //SLEEP
+            float Ts_fake = 0;
+            while (true){
                 auto finish = std::chrono::high_resolution_clock::now();
                 auto time = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
-                Ts_fake = (double)time / 1e3;
-                if(Ts_fake > 0.999)
+                
+                if(time > 1999)
                     break;
-            }*/
-
-            //RCLCPP_INFO(this->get_logger(), "time '%f'", Ts_fake);
-
-            //RCLCPP_INFO(this->get_logger(), "index '%f'", i);
+            }
+            
+            auto finish_total = std::chrono::high_resolution_clock::now();
+	        auto time_total = std::chrono::duration_cast<std::chrono::microseconds>(finish_total - start).count();
+            Ts_fake = (float)time_total / 1e6;
+            //RCLCPP_INFO(this->get_logger(), "time '%f'", time);
+            //RCLCPP_INFO(this->get_logger(), "Ts_fake '%f'", Ts_fake);
+            //RCLCPP_INFO(this->get_logger(), "Desired '%f'", vel_d_Right);
+            //RCLCPP_INFO(this->get_logger(), "Actual '%f'", speed_filtered_Right);
+            //RCLCPP_INFO(this->get_logger(), "vel left '%f'", speed_filtered_Left);
+            //RCLCPP_INFO(this->get_logger(), "index '%i'", i);
 
         }
 
-        //tickRight = 0;
-        //tickLeft = 0;
+
         
 
         //left.re_cancel();
         //right.re_cancel();
+        //forward(0,0);
+        //forward(1,0);
 
-        //RCLCPP_INFO(this->get_logger(), "finished '%f'", 0);
+        RCLCPP_INFO(this->get_logger(), "###########");
     }    
 
     
