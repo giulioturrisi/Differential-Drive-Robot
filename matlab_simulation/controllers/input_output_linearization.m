d0af2770577ca88d22af29f48413222182265ea7
Giulio <giulioturrisi@hotmail.it>
2021-07-29 11:51:59 +0200

    fixing input output matlab again

--- matlab_simulation/controllers/input_output_linearization.m
+++ matlab_simulation/controllers/input_output_linearization.m
@@ -12,7 +12,7 @@ rgbImage(int16(goal(1)*scale)+1,int16(goal(2)*scale+1),3) = 0;
 
 
 real_robot = [state_robot(1),state_robot(2),state_robot(3)];
-k1 = 10;
+k1 = 5;
 
 size_path = size(path);
 %main loop control
@@ -23,11 +23,17 @@ for d = 2:size_path(1)-1
     near_node = path(d,:);
 
         
-    %input-output lin
+    %feed forward velocity calculation
     x_vel = (near_node(1) - path(d - 1,1))/dt;
     y_vel = (near_node(2) - path(d - 1,2))/dt;
-    u1_io = k1*(near_node(1) - state_robot(1)) + x_vel;
-    u2_io = k1*(near_node(2) - state_robot(2)) + y_vel;
+    
+    %linearization point actual state
+    y1 = state_robot(1) + 0.02*cos(state_robot(3));
+    y2 = state_robot(2) + 0.02*sin(state_robot(3));
+    
+    %control law
+    u1_io = k1*(near_node(1) - y1) + x_vel;
+    u2_io = k1*(near_node(2) - y2) + y_vel;
     v = cos(state_robot(3))*u1_io + sin(state_robot(3))*u2_io;
     w = -sin(state_robot(3))*u1_io/0.02 + cos(state_robot(3))*u2_io/0.02;
     
@@ -51,6 +57,9 @@ for d = 2:size_path(1)-1
     rgbImage(int16(x*scale)+1,int16(y*scale)+1,3) = 255;
     
     real_robot = vertcat(real_robot,[state_robot(1),state_robot(2),state_robot(3)]);
+    
+    %to check evolution linearization point
+    %real_robot = vertcat(real_robot,[y1,y2,state_robot(3)]);
 
 outputArg1 = rgbImage;
 outputArg2 = real_robot;
