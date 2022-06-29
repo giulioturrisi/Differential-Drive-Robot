6d08d00cb7ad30c8b2543cde2f7eac6a31cac672
gturrisi <giulio.turrisi@iit.it>
2022-06-30 00:40:00 +0200

    fix A star python

--- python_simulation/planners/A_star/A_star.py
+++ python_simulation/planners/A_star/A_star.py
@@ -1,114 +1,169 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class A_star:
-    def __init__(self, start,goal,map,resolution):
+    def __init__(self, start, goal, map, resolution):
         self.resolution = resolution
         self.start = np.array([int(start[0]/resolution), int(start[1]/resolution)])
         self.goal = np.array([int(goal[0]/resolution), int(goal[1]/resolution)])
 
 
-        
-        map_shape = map.shape # 377,381
-        self.height = map_shape[0] #size_y - 377
-        self.width = map_shape[1] #size_x  - 381
+        map_shape = map.shape 
+        self.height = map_shape[0]  
+        self.width = map_shape[1] 
 
         
-        #to check here is wrong, should be self.width*self.height
-        self.map = np.zeros((self.width*self.width,6))
-        self.cells_isopen = np.zeros((self.width*self.width,3));
+
+        self.map = map
+
+        # node to expand
+        self.frontiers = []
+        self.frontiers.append([self.start[0], self.start[1], 0])
+
+        # node opened before
+        self.come_from = np.ones((self.height,self.width,2),int)*-1;
+        self.come_from[self.start[0]][self.start[1]] = [self.start[0],self.start[1]]
 
 
-        for i in range(0, self.height):
-            for j in range(0, self.width): 
-                if(map[i][j] < 250): 
-                    self.cells_isopen[i + j*self.width][:] = [-1,10000,10000];
-                else:
-                    h = 0;
-                    self.map[i+j*self.width][:] = [i,j,0,h,0,0];
-                    #print("index",i*self.height +j)
-                    
-                    if(i == self.start[0] and j == self.start[1]):
-                        #print("start at", i*self.width + j)
-                        self.cells_isopen[i + j*self.width][:] = [1,0,0];
-                    else:
-                        self.cells_isopen[i + j*self.width][:] = [0,10000,10000];
+        self.cost_so_far = np.ones((self.height,self.width,1),int)*1000;
+        self.cost_so_far[self.start[0]][self.start[1]] = 0
+
+        self.graph_cost = 1
+        
+
+        self.node_opened = []
 
 
     def find_next_cell(self,):
-        best_cost = 10000
-        best_cell = -1
-        for i in range(0, self.height):
-            for j in range(1, self.width):
-                index = i+j*self.width;
-                if(self.cells_isopen[index][0] == 1 and self.cells_isopen[index][1] <= best_cost):
-                    best_cost = self.cells_isopen[index][1]
-                    best_cell = index
-        return best_cell
+        best_node = []
+        best_cost = 1000
+        best_index = 0
+        for i in range(len(self.frontiers)):
+            temp = self.frontiers[i][:2]
+            temp_cost = self.frontiers[i][2]
+            if(temp_cost < best_cost):
+                best_cost = temp_cost
+                best_node = temp
+                best_index = i
+        self.frontiers.pop(best_index)
+        self.node_opened.append([best_node[0],best_node[1]])
+        return best_node
 
 
-    def check_goal(self,index):
-        x = self.map[index][0];
-        y = self.map[index][1];
-        if(x == self.goal[0] and y == self.goal[1]):
-            finish = 1;
-        else:
-            finish = 0;
-        return finish
+    def find_nearest_cell_to_goal(self,):
+        best_node = []
+        best_cost = 1000
+        for i in range(len(self.node_opened)):
+            temp = self.node_opened[i]
+            h = abs(self.goal[0]-(temp[0])) + abs(self.goal[1]-(temp[1]))
+            if(h < best_cost):
+                best_cost = h
+                best_node = temp
+        return best_node
 
 
-    def take_path(self,):
-        index = int(self.goal[0] + (self.goal[1])*self.width);
-        print("index",index)
-        path = np.ones((100,6));
-        dimension_path = 0;
-        for i in range(0,100):
-            path[i][0:2] = self.map[index][0:2]*self.resolution
-            index = int(self.map[index][5]);
-            dimension_path = dimension_path + 1
-            if(index == 0):
-                break
-        path = path[0:dimension_path][:]
-        return path
+    def open_sons_cell(self ,parent):
+        x_parent = parent[0];
+        y_parent = parent[1];
 
 
 
 
-    def open_sons_cell(self ,parent_index):
-        x = self.map[parent_index][0];
-        y = self.map[parent_index][1];
-
-        cost_parent = self.cells_isopen[parent_index][2];
-        self.cells_isopen[parent_index][0] = -1;
-        
         #need to check 8 sons!
         for i in range(-1,2):
             for j in range(-1,2):   
                 if(i == 0 and j == 0):
                     continue;
-                x_new = x + i
-                y_new = y + j
-                if(x_new<= self.width and y_new <= self.height):
-                    
-                    #calculate cost 
-                    graph_cost = 1               
-                    new_cost = cost_parent+graph_cost
+                x_new = x_parent + i
+                y_new = y_parent + j
 
-                    son_index = int(x_new+(y_new-1)*self.width)
+                print("parent", [x_parent, y_parent])
 
-                    
-                    if(self.cells_isopen[son_index][0] != -1 and self.cells_isopen[son_index][0] == 0):
-                        #print("different from -1")
-                        if (new_cost < self.cells_isopen[son_index][1]):
-                            h = abs(self.goal[0]-(x_new)) + abs(self.goal[1]-(y_new))
+                
 
-                            
-                            self.cells_isopen[son_index][2] = new_cost
-                            new_cost = new_cost + h
-                            
-                            #new_cost
-                            self.cells_isopen[son_index][1] = new_cost
-                            self.map[son_index][5] = int(parent_index)
-                            self.cells_isopen[son_index][0] = 1
+                if(x_new < self.height and y_new < self.width):
+                    if(self.map[x_new][y_new] != 254):
+                        break
+                    new_cost = self.cost_so_far[x_parent][y_parent] + self.graph_cost
+                    print("new node", [x_new, y_new])
+                    print("new cost", [new_cost])
+                    if(np.array_equal(self.come_from[x_new][y_new],np.array([-1,-1])) or new_cost < self.cost_so_far[x_new][y_new]):
+                        self.cost_so_far[x_new][y_new] = new_cost
+                        
+                        h = abs(self.goal[0]-(x_new)) + abs(self.goal[1]-(y_new))
+                        new_cost = new_cost + h
+                        self.frontiers.append([x_new, y_new, new_cost])     
+                        self.come_from[x_new][y_new] = [x_parent,y_parent]  # where i come from!      
+                        
+
+
+
+
+    def check_goal(self,next_cell):
+        x = next_cell[0];
+        y = next_cell[1];
+        return 1 if (x == self.goal[0] and y == self.goal[1]) else 0
+
+
+    def take_path(self, finish):
+        if(finish == 1):
+            last_cell = [self.goal[0],self.goal[1]];
+        else:
+            last_cell = self.find_nearest_cell_to_goal()
+
+        path = [last_cell]
+        last_cell = self.come_from[last_cell[0]][last_cell[1]]
+
+
+
+        for _ in range(self.width*self.width):
+            path.append(last_cell.tolist())
+            if(last_cell[0] == self.start[0] and last_cell[1] == self.start[1]):
+                break
+            last_cell = self.come_from[last_cell[0]][last_cell[1]]
+
+
+
+        print("path",path)
+        return path
+
+   
+
+
+
+
+    def plan(self, max_iteration, visualize=False):
+        finish = 0
+        iterator = 0
+
+        if(visualize):
+            rgb_image = np.stack([self.map]*3, axis=2)/255.
+            rgb_image[self.start[0]][self.start[1]] = [1,0,0]
+            rgb_image[self.goal[0]][self.goal[1]] = [0,0,1]
+            f = plt.figure()    
+
+
+        while (finish == 0 and iterator <= max_iteration):
+            next_cell = self.find_next_cell()
+            finish = self.check_goal(next_cell)
+            if(finish == 1):
+                break
+            self.open_sons_cell(next_cell)
+
+            iterator += 1
+
+            if(visualize):
+                rgb_image[next_cell[0]][next_cell[1]] = [0,1,0]
+                f.figimage(rgb_image)
+                plt.draw()
+                plt.pause(0.001)
+                rgb_image[next_cell[0]][next_cell[1]] = [1,0,0]
+
+
+        return self.take_path(finish)
+
+   
+
 
 
 
--- python_simulation/planners/A_star/planning_A_star.py
+++ python_simulation/planners/A_star/planning_A_star.py
@@ -3,22 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pgm_reader import Reader
 
-def planning_A_star(start,goal,map, resolution):
-    grid_search = A_star(start, goal, map, resolution)
-    finish = 0
-    while (finish == 0):
-        next_cell = grid_search.find_next_cell() 
-        if(next_cell == -1):
-            finish = 1
-            break
-        finish = grid_search.check_goal(next_cell)
-        if(finish == 1):
-            break
-        grid_search.open_sons_cell(next_cell)
-        
-    return grid_search.take_path()
-
-
 
 if __name__ == "__main__":
     f = './../../maps/map.pgm'
@@ -30,14 +14,21 @@ if __name__ == "__main__":
     print("height",height)
     
     ## initial state of the robot, goal position, hyperparameters
-    state_robot = np.array([9, 3, 0])  # x y theta
+    state_robot = np.array([10, 2.45, 0])  # x y theta
     dt = 0.1                           # sampling time
-    goal = np.array([10, 2.5])          # goal
+    goal = np.array([5, 2.5])          # goal
     max_iteration = 1000 #max iterations planner
     resolution = 0.05
 
 
+    print("state_robot",state_robot)
+    print("goal",goal)
+    print("image", np.shape(image))
 
-    path = planning_A_star(state_robot, goal, image, resolution)
+
+
+    grid_search = A_star(state_robot, goal, image, resolution)
+    path = grid_search.plan(max_iteration, False)
+
 
     print("path",path)
