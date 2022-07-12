fixing visualization problem zoom and rviz goal computation

python_simulation/planners/A_star/A_star.py
@@ -28,7 +28,7 @@ class A_star:
         self.cost_so_far = np.ones((self.height,self.width,1),int)*1000;
         self.cost_so_far[self.start[0]][self.start[1]] = 0
 
-        self.graph_cost = 1
+        self.graph_cost = 0.1
         
 
         self.node_opened = []
@@ -55,7 +55,8 @@ class A_star:
         best_cost = 1000
         for i in range(len(self.node_opened)):
             temp = self.node_opened[i]
-            h = abs(self.goal[0]-(temp[0])) + abs(self.goal[1]-(temp[1]))
+            #h = abs(self.goal[0]-(temp[0])) + abs(self.goal[1]-(temp[1]))
+            h = np.sqrt(np.power(self.goal[0]-(temp[0]),2) + np.power(self.goal[1]-(temp[1]),2))
             if(h < best_cost):
                 best_cost = h
                 best_node = temp
@@ -77,7 +78,7 @@ class A_star:
                 x_new = x_parent + i
                 y_new = y_parent + j
 
-                print("parent", [x_parent, y_parent])
+                #print("parent", [x_parent, y_parent])
 
                 
 
@@ -85,12 +86,13 @@ class A_star:
                     if(self.map[x_new][y_new] != 254):
                         break
                     new_cost = self.cost_so_far[x_parent][y_parent] + self.graph_cost
-                    print("new node", [x_new, y_new])
-                    print("new cost", [new_cost])
+                    #print("new node", [x_new, y_new])
+                    #print("new cost", [new_cost])
                     if(np.array_equal(self.come_from[x_new][y_new],np.array([-1,-1])) or new_cost < self.cost_so_far[x_new][y_new]):
                         self.cost_so_far[x_new][y_new] = new_cost
                         
-                        h = abs(self.goal[0]-(x_new)) + abs(self.goal[1]-(y_new))
+                        #h = abs(self.goal[0]-(x_new)) + abs(self.goal[1]-(y_new))
+                        h = np.sqrt(np.power(self.goal[0]-(x_new),2) + np.power(self.goal[1]-(y_new),2))
                         new_cost = new_cost + h
                         self.frontiers.append([x_new, y_new, new_cost])     
                         self.come_from[x_new][y_new] = [x_parent,y_parent]  # where i come from!      
@@ -124,7 +126,7 @@ class A_star:
 
 
 
-        print("path",path)
+        #print("path",path)
         return path
 
    
@@ -154,9 +156,9 @@ class A_star:
 
             if(visualize):
                 rgb_image[next_cell[0]][next_cell[1]] = [0,1,0]
-                f.figimage(rgb_image)
-                plt.draw()
-                plt.pause(0.001)
+                plt.imshow(rgb_image)
+                #plt.draw()
+                plt.pause(0.01)
                 rgb_image[next_cell[0]][next_cell[1]] = [1,0,0]
 
 

ros2_ws/src/planners/planners/run_planners.py
@@ -9,6 +9,9 @@ from nav_msgs.msg import OccupancyGrid, Path
 import tf_transformations
 
 import copy
+import matplotlib.pyplot as plt
+import math
+from scipy.interpolate import CubicSpline
 
 
 import sys
@@ -50,37 +53,65 @@ class Planners(Node):
     def goal_callback(self, msg):
         if(self.state_arrived == True and self.map_arrived == True):
             goal = np.array([msg.pose.position.x, msg.pose.position.y])
+            print("clicked point: ", goal)
+            print("state: ", self.state_robot)
             goal_shifted = np.zeros(2)
-            goal_shifted[0] = round(goal[0] - self.map_origin[0],1)
-            goal_shifted[1] = round(goal[1] - self.map_origin[1],1)
+            #goal_shifted[0] = np.shape(self.map)[1]*self.map_resolution - (goal[1] - self.map_origin[1])
+            goal_shifted[0] = goal[0] - self.map_origin[0]
+            goal_shifted[1] = goal[1] - self.map_origin[1]
 
             state_shifted = np.zeros(3)
-            state_shifted[0] = round(self.state_robot[0] - self.map_origin[0],1)
-            state_shifted[1] = round(self.state_robot[1] - self.map_origin[1],1)
+            state_shifted[0] = self.state_robot[0] - self.map_origin[0]
+            state_shifted[1] = self.state_robot[1] - self.map_origin[1]
+
 
             print("Start: ", state_shifted)
             print("Goal: ", goal_shifted)
 
-            planner = Breadth_First_Search(state_shifted, goal_shifted, self.map, round(self.map_resolution,2))
+            state_shifted[0] = round(state_shifted[0],1)
+            state_shifted[1] = round(state_shifted[1],1)
+
+
+            goal_shifted[0] = round(goal_shifted[0],1)
+            goal_shifted[1] = round(goal_shifted[1],1)
+
+
+
+            #DRAW
+            #state_rotated = np.zeros(3)
+            #angle = math.pi/2
+            #state_rotated[1] = state_shifted[0]*math.cos(angle) - math.sin(angle)*state_shifted[1]
+            #state_rotated[0] = state_shifted[0]*math.sin(angle) + math.cos(angle)*state_shifted[1]
+            #rgb_image = np.stack([self.map]*3, axis=2)/255.
+            #rgb_image[int(state_shifted[0]/self.map_resolution)][int(state_shifted[1]/self.map_resolution)] = [1,0,0]
+            #rgb_image[int(goal_shifted[0]/self.map_resolution)][int(goal_shifted[1]/self.map_resolution)] = [0,0,1]
+            #f = plt.figure() 
+            #plt.imshow(rgb_image)
+            #plt.show()
+            #return
+
+
+            planner = A_star(state_shifted, goal_shifted, self.map, round(self.map_resolution,2))
             path = planner.plan(self.max_iteration,self.visualize)
-            print("path", path)
+            
+            time = np.arange(0, len(path), 1)
+            spline = CubicSpline(time,np.array(path))
+            xs = np.arange(0, len(path), 0.1)
+
 
             path_msg = Path()
             path_msg.header.frame_id = "odom"
 
-            for i in range(np.shape(path)[0]):
-                temp = path.pop()
-                poseStamped = PoseStamped()
-                poseStamped.pose.position.x = temp[0] + self.map_origin[0]
-                poseStamped.pose.position.y = temp[1] + self.map_origin[1]
 
-                print("poseStamped",poseStamped)
-                print("poseStamped.pose.position.x",poseStamped.pose.position.x)
-                print("path[i][0]",path[i][0])
-                print("i",i)
-                print("path[i]:",temp)
+            for i in range(int(len(path)/0.1)):
+                temp = spline(xs[i])
+                poseStamped = PoseStamped()
+                poseStamped.pose.position.x = temp[0]*self.map_resolution + self.map_origin[0]
+                poseStamped.pose.position.y = temp[1]*self.map_resolution + self.map_origin[1]
+
                 poseStamped.header.frame_id = "odom"
                 path_msg.poses.append(poseStamped)
+
 
             self.publisher_path.publish(path_msg)
 
@@ -108,9 +139,13 @@ class Planners(Node):
         d = 0
         for i in range(msg.info.height):
             for j in range(msg.info.width):
-                self.map[i][j] = 255 if (msg.data[d] < 10 and msg.data[d] >= 0) else 0
-                self.map[i][j] = 255
+                self.map[i][j] = 254 if (msg.data[d] < 10 and msg.data[d] >= 0) else 0
+                #self.map[i][j] = 254
+                #self.map[i][j] = msg.data[d]
                 d = d+1
+
+        self.map = np.flip(self.map, 0)
+        self.map = np.rot90(self.map, axes=(1, 0))
 
         self.map_origin[0] = msg.info.origin.position.x
         self.map_origin[1] = msg.info.origin.position.y