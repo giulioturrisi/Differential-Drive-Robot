93f3669847e1b702b42b9e90ca80294964e37ecc
giulio <turrisigiulio@gmail.com>
2023-01-22 21:34:59 +0100

    adding robot publisher for rviz

---
+++ ros2_ws/src/state_estimation/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'state_estimation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('../../coppeliasim_simulation/urdf_from_coppelia/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'state_publisher = state_estimation.state_publisher:main'
+        ],
+    },
+)
---
+++ ros2_ws/src/state_estimation/state_estimation/state_publisher.py
@@ -0,0 +1,94 @@
+from math import sin, cos, pi
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import JointState
+from tf2_ros import TransformBroadcaster, TransformStamped
+
+from geometry_msgs.msg import TwistStamped
+
+class StatePublisher(Node):
+
+    def __init__(self):
+        #rclpy.init()
+        super().__init__('state_publisher')
+
+        qos_profile = QoSProfile(depth=10)
+        self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
+        self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
+        self.nodeName = self.get_name()
+        self.get_logger().info("{0} started".format(self.nodeName))
+
+        self.subscription_odom = self.create_subscription(TwistStamped, 'odometry', self.odometry_callback,1)
+        self.create_timer(0.005, self.tf_callback)
+
+        self.last_time = 0.0
+
+        self.odom_x = 0.0
+        self.odom_y = 0.0
+        self.odom_theta = 0.0
+
+
+
+    def tf_callback(self):
+        # message declarations
+        odom_trans = TransformStamped()
+        odom_trans.header.frame_id = 'odom'
+        odom_trans.child_frame_id = 'base_footprint'
+        joint_state = JointState()
+        # update joint_state
+        now = self.get_clock().now()
+        joint_state.header.stamp = now.to_msg()
+        joint_state.name = ['joint_left', 'joint_right']
+        joint_state.position = [0.0, 0.0]
+
+        # update transform
+        odom_trans.header.stamp = now.to_msg()
+        odom_trans.transform.translation.x = self.odom_x #self.odom_x
+        odom_trans.transform.translation.y = self.odom_y #self.odom_y
+        odom_trans.transform.translation.z = 0.05 
+        odom_trans.transform.rotation = \
+            euler_to_quaternion(0, 0, self.odom_theta) # roll,pitch,yaw #self.odom_theta
+
+        # send the joint state and transform
+        self.joint_pub.publish(joint_state)
+        self.broadcaster.sendTransform(odom_trans)
+
+       
+
+    def odometry_callback(self, msg):
+        print("odometry received")
+
+        v_reconstructed = msg.twist.linear.x 
+        w_reconstructed = msg.twist.angular.z 
+        time = msg.header.stamp.nanosec/1000000000.00
+
+        if(self.last_time != 0.0):
+            Ts = time - self.last_time    
+            print("Ts", Ts)
+            if(Ts > 0.0):
+                self.odom_x = self.odom_x + v_reconstructed*Ts*cos(self.odom_theta);
+                self.odom_y = self.odom_y + v_reconstructed*Ts*sin(self.odom_theta);
+                self.odom_theta = self.odom_theta + w_reconstructed*Ts
+
+        self.last_time = time
+
+
+def euler_to_quaternion(roll, pitch, yaw):
+    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+    return Quaternion(x=qx, y=qy, z=qz, w=qw)
+
+def main():
+    rclpy.init()
+    node = StatePublisher()
+
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
